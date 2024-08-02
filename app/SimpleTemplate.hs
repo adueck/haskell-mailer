6b28a7df13b7d399cd5a9054c48458df7cb5f49e
adueck <adueck@users.noreply.github.com>
2024-08-02 11:35:28 -0400

    using text replace instead of regex because regex causes issue on server

--- app/SimpleTemplate.hs
+++ app/SimpleTemplate.hs
@@ -3,8 +3,7 @@ module SimpleTemplate
   )
 where
 
-import Data.Text (Text, pack)
-import Text.Regex (mkRegex, subRegex)
+import Data.Text (Text, pack, replace)
 
 -- | takes a path to a text file, and a list of (key,value) pairs
 -- for replacing replaces each ${key} in the file with it's value
@@ -12,8 +11,7 @@ renderTemplate :: FilePath -> [(String, String)] -> IO Text
 renderTemplate path replacements = do
   htmlTemplate <- readFile path
   return $
-    pack $
-      foldl
-        (\temp (key, val) -> subRegex (mkRegex $ "{{" ++ key ++ "}}") temp val)
-        htmlTemplate
-        replacements
+    foldl
+      (\temp (key, val) -> replace (pack ("{{" ++ key ++ "}}")) (pack val) temp)
+      (pack htmlTemplate)
+      replacements
